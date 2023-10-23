624916cf071fbf57545339996ab0e2e13898b976
JorgeSicsu <jlsicsu@hotmail.com>
2023-10-23 15:31:56 -0300

    Projeto API-produto-curso-main-231023

--- src/main/java/br/com/demoapi/config/OpenAPIConfig.java
+++ src/main/java/br/com/demoapi/config/OpenAPIConfig.java
@@ -1,11 +1,14 @@
 package br.com.demoapi.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "/demoapi/", description = "api-produto-curso-main-production.up.railway.app")})
 public class OpenAPIConfig {
 
     @Bean
